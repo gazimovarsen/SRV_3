5055a9ec647c44332711565685342425c77dae44
gazimovarsen <gazimovarsen@gmail.com>
2024-10-07 01:48:03 +0500

    Добавьте файлы проекта.

---
+++ SRV_3/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SRV_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inputPrimary = new System.Windows.Forms.TextBox();
+            this.runPrimary = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.inputSecondary = new System.Windows.Forms.TextBox();
+            this.runSecondary = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputPrimary
+            // 
+            resources.ApplyResources(this.inputPrimary, "inputPrimary");
+            this.inputPrimary.Name = "inputPrimary";
+            // 
+            // runPrimary
+            // 
+            resources.ApplyResources(this.runPrimary, "runPrimary");
+            this.runPrimary.Name = "runPrimary";
+            this.runPrimary.UseVisualStyleBackColor = true;
+            this.runPrimary.Click += new System.EventHandler(this.runPrimary_Click);
+            // 
+            // outputTextBox
+            // 
+            resources.ApplyResources(this.outputTextBox, "outputTextBox");
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            // 
+            // inputSecondary
+            // 
+            resources.ApplyResources(this.inputSecondary, "inputSecondary");
+            this.inputSecondary.Name = "inputSecondary";
+            // 
+            // runSecondary
+            // 
+            resources.ApplyResources(this.runSecondary, "runSecondary");
+            this.runSecondary.Name = "runSecondary";
+            this.runSecondary.UseVisualStyleBackColor = true;
+            this.runSecondary.Click += new System.EventHandler(this.runSecondary_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.runSecondary);
+            this.Controls.Add(this.inputSecondary);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.runPrimary);
+            this.Controls.Add(this.inputPrimary);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_LoadAsync);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputPrimary;
+        private System.Windows.Forms.Button runPrimary;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.TextBox inputSecondary;
+        private System.Windows.Forms.Button runSecondary;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SRV_3/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SRV_3
+{
+    public partial class Form1 : Form
+    {
+        private NamedPipeServerStream pipeServer;
+        private Process cmdProcess;
+        public string serverMessage = "Standard message!";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+            await RunPipeServerAsync();
+        }
+
+        private void runPrimary_Click(object sender, EventArgs e)
+        {
+            StartCmdProcess(inputPrimary.Text);
+        }
+
+        private void runSecondary_Click(object sender, EventArgs e)
+        {
+            serverMessage = inputSecondary.Text;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Cleanup();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cleanup();
+        }
+
+        private void Cleanup()
+        {
+            if (cmdProcess != null && !cmdProcess.HasExited)
+            {
+                cmdProcess.Kill();
+            }
+            pipeServer?.Close();
+        }
+
+        private async Task RunPipeServerAsync()
+        {
+            pipeServer = new NamedPipeServerStream("testPipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+            Console.WriteLine("Waiting for client connection...");
+            await pipeServer.WaitForConnectionAsync();
+            Console.WriteLine("Client connected.");
+
+            // Handle communication with the connected client in separate tasks
+            _ = HandleClientReadingAsync(pipeServer);
+            _ = HandleClientWritingAsync(pipeServer);
+        }
+
+
+        static async Task HandleClientReadingAsync(NamedPipeServerStream server)
+        {
+            using (StreamReader reader = new StreamReader(server, Encoding.UTF8))
+            {
+                while (server.IsConnected)
+                {
+                    string clientMessage = await reader.ReadLineAsync();
+                    if (clientMessage != null)
+                    {
+                        // Console.WriteLine("Received from client: " + clientMessage);
+                    }
+                }
+            }
+        }
+
+        async Task HandleClientWritingAsync(NamedPipeServerStream server)
+        {
+            using (StreamWriter writer = new StreamWriter(server, Encoding.UTF8) { AutoFlush = true })
+            {
+                while (server.IsConnected)
+                {
+                    await writer.WriteLineAsync(serverMessage);
+                    // Console.WriteLine("Sent to client: " + serverMessage);
+
+                    // Delay to simulate some processing or wait for a response
+                    await Task.Delay(1000); // Отправляем сообщение каждую секунду
+                }
+            }
+        }
+
+        private void StartCmdProcess(string command)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c {command}",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            cmdProcess = new Process { StartInfo = startInfo };
+            cmdProcess.OutputDataReceived += (s, e) => AppendOutput(e.Data);
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+
+            if (pipeServer.IsConnected)
+            {
+                AppendOutput("Канал подключен к процессу.");
+            }
+        }
+
+        private void AppendOutput(string text)
+        {
+            if (outputTextBox.InvokeRequired)
+            {
+                outputTextBox.Invoke(new Action<string>(AppendOutput), text);
+            }
+            else
+            {
+                outputTextBox.AppendText(text + Environment.NewLine);
+            }
+        }
+    }
+}
